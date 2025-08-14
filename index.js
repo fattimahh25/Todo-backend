e74b9851372abd8f992c9a2c16814890dff40e4c
fatima <influncexere@gmail.com>
2025-08-14 16:45:01 +0500

    second update

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-const todoRoutes = require('./routes/todos,'); 
+const todoRoutes = require('./routes/todos'); 
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth/', authRoutes);
