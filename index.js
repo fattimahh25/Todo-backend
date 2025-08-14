aa582369b22b6390b56cc9c1b6722cdfde5aa746
fatima <influncexere@gmail.com>
2025-08-14 17:05:16 +0500

    fourth update

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const authRoutes = require('./routes/auth');
 const todoRoutes = require('./routes/todos'); 
 
 const app = express();
+const PORT = process.env.PORT || 5000; // Store port in a variable
+
 app.use(cors());
 app.use(express.json());
 
+console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
+
 app.use('/api/auth/', authRoutes);
 app.use('/api/todos/', todoRoutes);
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   })
   .catch(err => console.error(err));
